a41cb12def37a64e3e5859a1f44a9dcb8744590e
ibraheemalsaady <ibraheem.al-saady@outlook.com>
2020-06-27 23:12:15 +0200

    added script and fixed index.yaml

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Helm Charts
+This repo contains custom helm charts
+
+## Usage
+`helm repo add kubechamp  https://kube-champ.github.io/helm-charts/`
+
---
+++ scripts/helm-prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo ">>> Generate helm packages..."
+helm package charts/*
+
+mv *.tgz packaged
+
+echo ">>> Generating new index.yaml"
+helm repo index --url https://github.com/kube-champ/helm-charts/packaged .
